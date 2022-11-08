ac206a35e1841c363fb243f7a0277b8d4294eca6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-11-08 20:33:34 +0200

    Import  common module and router module

--- photo-gallery-app/src/app/app.component.ts
+++ photo-gallery-app/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
